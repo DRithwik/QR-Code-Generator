511a12fd641b84797725c0de2300fb28157acb98
Rithwik Deshaboina <168298268+DRithwik@users.noreply.github.com>
2025-01-04 03:30:17 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        <a className={styles.anchor} href="https://github.com/DRithwik">
+          Rithwik Deshaboina
+        </a>
+      </p>
+    </footer>
+  );
+}
